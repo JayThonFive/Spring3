10237f68b10ce09c1ac1e64b26fa1829291a64d0
johal <almimago2708@gmail.com>
2022-09-17 15:11:13 -0500

    Controlador Enterprise

---
+++ src/main/java/com/example/AplicativoWeb/Controlador/ControlEnterprise.java
@@ -0,0 +1,33 @@
+package com.example.aplicativoWed.Controller;
+
+import com.example.aplicativoWed.Entidades.Enterprise;
+import com.example.aplicativoWed.Servicios.ServiEnterpriseImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ControlEnterprise {
+
+    @Autowired
+    ServiEnterpriseImp serviEnterpriseImp;
+
+    @GetMapping("/empresa")
+    private List<Enterprise> verEmpresa(){
+        return serviEnterpriseImp.verEmpresa();
+    }
+    @PutMapping("/empresa")
+    private void actualizarEmpresa (@RequestBody Enterprise enterprise){
+        serviEnterpriseImp.editarEmpresa(enterprise);
+    }
+    @PostMapping ("/empresa")
+    private void crearEmpresa(@RequestBody Enterprise enterprise){
+        serviEnterpriseImp.crearEmpresa(enterprise);
+    }
+    @DeleteMapping("/empresa/{id}")
+    private void eliminarEmpresa (@PathVariable("id")Integer id){
+        serviEnterpriseImp.eliminarEmpresa(id);
+    }
+
+}
