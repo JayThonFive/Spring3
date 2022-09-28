89f42b1b0e86ce301119575aa644759a630e8dab
johal <almimago2708@gmail.com>
2022-09-28 09:01:26 -0500

    1.7 con html sin css

--- src/main/java/com/example/AplicativoWeb/Controlador/ControlEmpresa.java
+++ src/main/java/com/example/AplicativoWeb/Controlador/ControlEmpresa.java
@@ -3,12 +3,12 @@ package com.example.AplicativoWeb.Controlador;
 import com.example.AplicativoWeb.Entidades.Empresa;
 import com.example.AplicativoWeb.Servicio.ServiEmpresaImp;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-@RestController
+@Controller
 public class ControlEmpresa {
 
     @Autowired
@@ -17,26 +17,26 @@ public class ControlEmpresa {
     @GetMapping("/enterprise")
     private String verEmpresa(Model model){
         model.addAttribute("enterprises",serviEmpresaImp.verEmpresa());
-        return "enterprise";
+        return "empresa";
     }
     @GetMapping("/enterprise/{id}")
     private void empresaId (@PathVariable("id")Long Long) {
         serviEmpresaImp.empresaId(Long);
     }
 
-    @PutMapping("/enterprise")
-    private void actualizarEmpresa (@RequestBody Empresa empresa){
-        serviEmpresaImp.editarEmpresa(empresa);
-    }
     @GetMapping("/agregarEmpresa")
     private String formularioEmpresa (Empresa empresa){
         return "agregarEmpresa";
     }
-
     @PostMapping ("/enterprise")
     private String crearEmpresa(Empresa empresa){
         serviEmpresaImp.crearEmpresa(empresa);
-        return "redirect:/enterprise";
+        return "redirect:/empresa";
+    }
+
+    @PutMapping("/enterprise")
+    private void actualizarEmpresa (@RequestBody Empresa empresa){
+        serviEmpresaImp.editarEmpresa(empresa);
     }
     @DeleteMapping("/enterprise/{id}")
     private void eliminarEmpresa (@PathVariable("id")Long id){
